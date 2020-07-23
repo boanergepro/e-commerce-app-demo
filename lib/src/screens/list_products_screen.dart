0065268f39e42240d1a76a0bb5f2aeaad34ccb90
boanergepro <antonycarrizo96@gmail.com>
2020-07-23 15:56:33 -0400

    [MOD]: - Agregue los productos.

--- lib/src/screens/list_products_screen.dart
+++ lib/src/screens/list_products_screen.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerceapp/src/widgets/custom_app_bar.dart';
 import 'package:ecommerceapp/src/widgets/custom_botttom_bar.dart';
+import 'package:ecommerceapp/src/widgets/item_product.dart';
 import 'package:flutter/material.dart';
 
 class ListProductsScreen extends StatefulWidget {
@@ -10,11 +11,65 @@ class ListProductsScreen extends StatefulWidget {
 }
 
 class _ListProductsScreenState extends State<ListProductsScreen> {
+  List<ItemProduct> items = [
+    ItemProduct(
+      name: 'HandBag red',
+      price: '30',
+      urlImage: 'assets/handbag_red.png',
+      favorite: true,
+    ),
+    ItemProduct(
+      name: 'South Hoodie',
+      price: '50',
+      urlImage: 'assets/south_hoodie.png',
+      favorite: true,
+    ),
+    ItemProduct(
+      name: 'Dress blue',
+      price: '25',
+      urlImage: 'assets/dress_blue_retro.png',
+      favorite: false,
+    ),
+    ItemProduct(
+      name: 'Women Shoe',
+      price: '50',
+      urlImage: 'assets/black_women_shoe.png',
+      favorite: false,
+    ),
+    ItemProduct(
+      name: 'Blue Jean',
+      price: '30',
+      urlImage: 'assets/calvin_klein_jean.png',
+      favorite: true,
+    ),
+    ItemProduct(
+      name: 'Short red',
+      price: '18',
+      urlImage: 'assets/short_red.png',
+      favorite: true,
+    ),
+    ItemProduct(
+      name: 'Classic men shoe',
+      price: '40',
+      favorite: false,
+      urlImage: 'assets/classic_black_men_shoes.png',
+    ),
+    ItemProduct(
+      name: 'Boots',
+      price: '18',
+      urlImage: 'assets/boots.png',
+      favorite: true,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    final double itemHeight = (size.height - kToolbarHeight - 24) * 0.25;
+    final double itemWidth = size.width / 2;
 
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: customAppBar(
           title: 'New Trend',
           ctx: context,
@@ -45,8 +100,22 @@ class _ListProductsScreenState extends State<ListProductsScreen> {
                 ),
               ],
             ),
-          )),
-      body: Container(),
+          ),
+      ),
+      body: Container(
+        margin: EdgeInsets.only(left: 8, top: 15, right: 8),
+        child: GridView.count(
+          crossAxisSpacing: 8,
+          crossAxisCount: 2,
+          childAspectRatio: (itemWidth / itemHeight),
+          controller: new ScrollController(keepScrollOffset: false),
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+          children: List.generate(items.length, (index) {
+            return items[index];
+          }),
+        ),
+      ),
       bottomNavigationBar: CustomNavigationBar(
         backgroundColor: Colors.white,
         children: <NavigationBarItem>[
---
+++ lib/src/widgets/item_product.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class ItemProduct extends StatelessWidget {
+  String urlImage;
+  String name;
+  String price;
+  bool favorite;
+
+  ItemProduct({
+    this.urlImage,
+    this.name,
+    this.price,
+    this.favorite,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Container(
+          child: Stack(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Container(
+                    height: constraints.maxHeight * 0.5,
+                  ),
+                  Container(
+                    height: constraints.maxHeight * 0.5,
+                    child: Card(
+                      elevation: 4,
+                      child: Container(
+                        alignment: Alignment.bottomCenter,
+                        width: constraints.maxWidth,
+                        child: ListTile(
+                          title: Text(
+                            name,
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          subtitle: Text(
+                            '\$$price',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          trailing: Icon(
+                            favorite ? Icons.favorite : Icons.favorite_border,
+                            color: favorite ? Colors.red : Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 20,
+                ),
+                height: constraints.maxHeight * 0.5,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(urlImage),
+                  )
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
